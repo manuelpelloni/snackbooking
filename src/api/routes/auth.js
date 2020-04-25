278c7a0223b3ec01f6cbd9e6e6c4c02a6d3e34e2
manuelpelloni <61501986+manuelpelloni@users.noreply.github.com>
2020-04-25 03:23:42 +0200

    Improved error handling, removed defaults from request parameters

--- src/api/routes/auth.js
+++ src/api/routes/auth.js
@@ -6,13 +6,13 @@ const validator = require('validator');
 const sql = require('mssql');
 
 router.post('/signup', async (req, res, next) => {
-    const class_number = req.body.class || 1;
-    const section =  req.body.section || 'A';
-    const admin = req.body.admin || true;
-    const email = req.body.email || 'pellonimanuel@gmail.com';
-    const digest = req.body.password_digest || 'a';
+    const class_number = req.body.class;
+    const section = req.body.section;
+    const admin = req.body.admin;
+    const email = req.body.email;
+    const password = req.body.password;
 
-    if(!validator.isEmail(email)){
+    if (!validator.isEmail(email)) {
         return res.status(403).json(
             {
                 status: 403,
@@ -21,35 +21,42 @@ router.post('/signup', async (req, res, next) => {
         );
     };
 
-    bcrypt.genSalt(12, function(err, salt) {
-        bcrypt.hash(digest, salt, function(err, hash) {
-            if(err){
+    bcrypt.genSalt(12, function (err, salt) {
+        if(err){
+            return res.status().json({
+                message: 'Something gone wrong with yout password, please try again'
+            });
+        }
+        bcrypt.hash(password, salt, function (err, hash) {
+            if (err) {
                 return res.status().json({
-                    message : 'Something gone wrong with yout password, please try again'
+                    message: 'Something gone wrong with yout password, please try again'
                 });
             }
             db.createQuery()
                 .input('class_number', sql.Int, class_number)
                 .input('section', sql.Char, section)
                 .input('email', sql.VarChar, email)
-                .input('password_digest', sql.VarChar, password_digest)
+                .input('password_digest', sql.VarChar, hash)
                 .query('INSERT INTO users(class_number, section, email, password_digest)\
                         VALUES(@class_number, @section, @email, @password_digest)')
                 .then(result => {
                     console.log(result);
                     res.send.json({
-                        message : 'Success, account Created!'
-                    }); 
+                        //TODO-----------------
+                        message: 'Success, account Created!'
+                    });
                 })
                 .catch(err => {
                     res.json({
-                        message : 'Something gone wrong, please try signup again',
-                        err : err
-                    }); 
+                        message: 'Something gone wrong, please try signup again',
+                        err: err
+                    });
                 });
         });
     });
-;})
+    ;
+})
 
 
 module.exports = router;
