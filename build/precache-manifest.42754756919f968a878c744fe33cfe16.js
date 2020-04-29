55589963ceb909f3569e458b408eede3050fca3c
manuelpelloni <61501986+manuelpelloni@users.noreply.github.com>
2020-04-29 17:29:13 +0200

    Run create-react-app

---
+++ build/precache-manifest.42754756919f968a878c744fe33cfe16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e816b2941205f3372040cf8c69d9bb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5dd148db76e05ab6fb8",
+    "url": "/static/css/main.c80ad70b.chunk.css"
+  },
+  {
+    "revision": "c959abe4bc915778b2cc",
+    "url": "/static/js/2.5affc270.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5affc270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5dd148db76e05ab6fb8",
+    "url": "/static/js/main.2f9144b1.chunk.js"
+  },
+  {
+    "revision": "1d68412ce111427c2818",
+    "url": "/static/js/runtime-main.e71bf732.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
