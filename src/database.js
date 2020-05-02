cf9cf8c5c71b13dd51e407d53aeead32ab6189fd
manuelpelloni <61501986+manuelpelloni@users.noreply.github.com>
2020-05-02 03:33:08 +0200

    Change cookie maxAge, add validateCredentialsAndLogin and userFromToken functions

--- src/database.js
+++ src/database.js
@@ -1,4 +1,7 @@
 const sql = require("mssql");
+const bcrypt = require("bcrypt");
+const { v4: uuidv4 } = require("uuid");
+const cookieSessionValidity = 60 * 60 * 24 * 30; // 30 days
 
 const config = {
   connectionLimit: process.env.CONNECTION_LIMIT,
@@ -17,7 +20,7 @@ function createQuery() {
   return pool.request();
 }
 
-async function tokenValidator(res, token) {
+async function userFromToken(res, token) {
   const result = await db
     .createQuery()
     .input("token", sql.validator, token)
@@ -31,4 +34,36 @@ async function tokenValidator(res, token) {
   return result.recordset[0];
 }
 
-module.exports = { createQuery, tokenValidator };
+async function validateCredentialsAndLogin() {
+  const { email, password } = req.body;
+  const result = await db
+    .createQuery()
+    .input("email", sql.VarChar, email)
+    .query(
+      "SELECT id AS user_id, password_digest FROM users WHERE email = @email"
+    );
+
+  const { user_id, password_digest } = result.recordset[0];
+
+  const match = await bcrypt.compare(password, password_digest);
+  if (match) {
+    const token = uuidv4();
+    await db
+      .createQuery()
+      .input("id", sql.VarChar, token)
+      .input("user_id", sql.Int, user_id)
+      .query("INSERT INTO sessions(id, user_id) VALUES(@id, @user_id)");
+
+    res.cookie("session", token, {
+      maxAge: cookieSessionValidity,
+      httpOnly: true,
+    });
+    return res.send("");
+  } else {
+    return res.json({
+      message: "Devi registrarti prima di fare il login",
+    });
+  }
+}
+
+module.exports = { createQuery, userFromToken, validateCredentialsAndLogin };
