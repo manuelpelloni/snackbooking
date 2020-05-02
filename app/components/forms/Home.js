cf0b06d489d09cfb202495eb1e4fb97185627d54
manuelpelloni <61501986+manuelpelloni@users.noreply.github.com>
2020-05-02 04:35:12 +0200

    Removed submenu from drop down menu

--- app/components/forms/Home.js
+++ app/components/forms/Home.js
@@ -40,20 +40,6 @@ const Home = () => {
         <Menu.Item key="3" icon={<ContainerOutlined />}>
           Option 3
         </Menu.Item>
-        <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
-          <Menu.Item key="5">Option 5</Menu.Item>
-          <Menu.Item key="6">Option 6</Menu.Item>
-          <Menu.Item key="7">Option 7</Menu.Item>
-          <Menu.Item key="8">Option 8</Menu.Item>
-        </SubMenu>
-        <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
-          <Menu.Item key="9">Option 9</Menu.Item>
-          <Menu.Item key="10">Option 10</Menu.Item>
-          <SubMenu key="sub3" title="Submenu">
-            <Menu.Item key="11">Option 11</Menu.Item>
-            <Menu.Item key="12">Option 12</Menu.Item>
-          </SubMenu>
-        </SubMenu>
       </Menu>
     </div>
   );
