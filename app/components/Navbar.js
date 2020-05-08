f2b8c2ad5935144a7e6f5700afa4939b9b0e00de
manuelpelloni <61501986+manuelpelloni@users.noreply.github.com>
2020-05-08 23:48:06 +0200

    run prettier

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -4,24 +4,23 @@ import "./Navbar.css";
 import Logo from "../navbarHome.svg";
 
 const Navbar = () => {
-
   return (
     <ul className="Navbar">
       <li className="pages">
         <Link to="/" className="link-color">
-          <img src={Logo} className="navbar-icon" />
+          <img src={Logo} className="navbar-icon" alt="panini" />
           <span>panini</span>
         </Link>
       </li>
       <li className="pages">
         <Link to="/" className="link-color">
-          <img src={Logo} className="navbar-icon" />
+          <img src={Logo} className="navbar-icon" alt="placeholder" />
           <span>placeholder</span>
         </Link>
       </li>
       <li className="pages">
         <Link to="/" className="link-color">
-          <img src={Logo} className="navbar-icon" />
+          <img src={Logo} className="navbar-icon" alt="placeholder" />
           <span>placeholder</span>
         </Link>
       </li>
