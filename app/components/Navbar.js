ea4a3cf8941acc3dab7e546b898df4f95757c318
manuelpelloni <61501986+manuelpelloni@users.noreply.github.com>
2020-05-08 11:04:10 +0200

    Add navbar component

---
+++ app/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
+
+export default Navbar;
