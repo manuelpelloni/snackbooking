4bb1ac8f6c43240eec4f43a3e9147c1635c4ef8f
manuelpelloni <61501986+manuelpelloni@users.noreply.github.com>
2020-05-08 23:27:57 +0200

    bottom navbar refactor

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,12 +1,32 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Navbar.css";
+import Logo from "../navbarHome.svg";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="Navbar">
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
+    <ul className="Navbar">
+      <li className="pages">
+        <Link to="/" className="link-color">
+          <img src={Logo} className="navbar-icon" />
+          <span>panini</span>
+        </Link>
+      </li>
+      <li className="pages">
+        <Link to="/" className="link-color">
+          <img src={Logo} className="navbar-icon" />
+          <span>placeholder</span>
+        </Link>
+      </li>
+      <li className="pages">
+        <Link to="/" className="link-color">
+          <img src={Logo} className="navbar-icon" />
+          <span>placeholder</span>
+        </Link>
+      </li>
+    </ul>
   );
 };
 
