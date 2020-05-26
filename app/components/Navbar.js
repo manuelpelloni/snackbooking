64ffccc6bca776552a50e74e76a09ba88fac24d6
manuelpelloni <61501986+manuelpelloni@users.noreply.github.com>
2020-05-26 17:00:20 +0200

    Bottom navbar refactor changed icons, imported icons from fontawesome css restyling

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,27 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import Logo from "../navbarHome.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faShoppingBag,
+  faPizzaSlice,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <ul className="Navbar">
       <li className="pages">
         <Link to="/" className="link-color">
-          <img src={Logo} className="navbar-icon" alt="panini" />
-          <span>panini</span>
+          <FontAwesomeIcon icon={faPizzaSlice} size="2x" />
+          <span className="icon-description">Panini</span>
         </Link>
       </li>
       <li className="pages">
         <Link to="/" className="link-color">
-          <img src={Logo} className="navbar-icon" alt="placeholder" />
-          <span>placeholder</span>
+          <FontAwesomeIcon icon={faShoppingBag} size="2x" />
+          <span className="icon-description">Carrello</span>
         </Link>
       </li>
       <li className="pages">
         <Link to="/" className="link-color">
-          <img src={Logo} className="navbar-icon" alt="placeholder" />
-          <span>placeholder</span>
+          <FontAwesomeIcon icon={faUser} size="2x" />
+          <span className="icon-description">Utente</span>
         </Link>
       </li>
     </ul>
