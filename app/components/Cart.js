e46509fefe128276b16c0c9d37eeeeb28857a2ee
manuelpelloni <61501986+manuelpelloni@users.noreply.github.com>
2020-05-28 00:41:18 +0200

    Add CartItem component

--- app/components/Cart.js
+++ app/components/Cart.js
@@ -1,8 +1,26 @@
-import React from "react";
+import { React, useEffect } from "react";
 import "./Cart.css";
 import Navbar from "./Navbar";
+import request from "../../utils/http";
 
 const Cart = () => {
+  const [items, setitems] = useState([]);
+
+  async function fetchItems() {
+    const response = await request("GET", "api/orders");
+    const data = await response.json();
+    setitems(data);
+  }
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const components = [];
+  for (const item of items) {
+    components.push(<CartItem key={item.id} product={item} />);
+  }
+
   return (
     <div className="Cart">
       <div className="items-container "></div>
---
+++ app/components/CartItem.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./CartItem.css";
+
+const CartItem = () => {
+  
+}
