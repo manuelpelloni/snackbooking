9f49bda81a76e4cb7d388ba38e5fc1909be546c2
manuelpelloni <61501986+manuelpelloni@users.noreply.github.com>
2020-05-28 15:47:16 +0200

    Component restyling

--- app/components/CartItem.js
+++ app/components/CartItem.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./CartItem.css";
 
-const CartItem = () => {
-  
-}
+const CartItem = ({ item }) => {
+  return (
+    <div>
+      <button className="CartItem" onClick="">
+        <span>{item.name}</span>
+        <input type="number" value={item.quantity} />
+      </button>
+    </div>
+  );
+};
+
+export default CartItem;
