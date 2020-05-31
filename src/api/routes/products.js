9e7da8ab9fd233d1c20b00927e8eb2d71d48c661
manuelpelloni <61501986+manuelpelloni@users.noreply.github.com>
2020-05-31 05:03:52 +0200

    Add route to add an item to the cart

--- src/api/routes/products.js
+++ src/api/routes/products.js
@@ -82,4 +82,48 @@ router.delete("/:id", async (req, res) => {
   res.send();
 });
 
+router.post("/add-to-cart", async (req, res) => {
+  const user = await db.userFromRequest(req);
+  if (!user) return res.status(401).json({ message: "Devi prima loggarti" });
+  const { product_id } = req.body;
+  console.log(product_id);
+
+  const result = await db
+    .createQuery()
+    .input("product_id", sql.Int, product_id)
+    .input("user_id", sql.Int, user.id)
+    .query(
+      "SELECT orders_products.quantity as product_quantity, orders.id as order_id\
+            FROM orders_products\
+                join orders on orders.id = orders_products.order_id\
+            WHERE orders.user_id = @user_id and orders_products.product_id = @product_id"
+    );
+
+  try {
+    const { product_quantity, order_id } = result.recordset[0];
+    console.log(product_quantity, order_id);
+    if (product_quantity) {
+      await db
+        .createQuery()
+        .input("product_id", sql.Int, product_id)
+        .input("order_id", sql.Int, order_id)
+        .input("product_quantity", sql.Int, product_quantity + 1)
+        .query(`UPDATE orders_products\
+        SET quantity =  @product_quantity\
+        WHERE order_id = @order_id and product_id = @product_id`);
+    }
+
+    res.json({
+      message: "Aggiunto al carrello",
+      added: true,
+    });
+  } catch (err) {
+    res.json({
+      err: err.message,
+      message: "Non ggiunto al carrello",
+      added: false,
+    });
+  }
+});
+
 module.exports = router;
