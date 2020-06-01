688bd07f33cc79a63571dfbe676b0bd7ea8d6d0f
manuelpelloni <61501986+manuelpelloni@users.noreply.github.com>
2020-06-01 09:56:36 +0200

    Implement removing one item from cart Calling remove-one-from-cart endpoint and change item pieces graphically

--- app/components/CartItem.js
+++ app/components/CartItem.js
@@ -11,11 +11,11 @@ import request from "../utils/http";
 const CartItem = ({ item }) => {
   const [quantity, setQuantity] = useState(item.quantity);
   const [visible, setVisible] = useState(true);
+  const body = {
+    product_id: item.product.id,
+  };
 
   async function addItemToDB() {
-    const body = {
-      product_id: item.product.id,
-    };
     const { added, message } = await request(
       "POST",
       "api/products/add-to-cart",
@@ -23,10 +23,15 @@ const CartItem = ({ item }) => {
     );
     if (added) console.log("implementa sti cazzo di alert", message);
   }
+  async function removeOneItemFromDB() {
+    const { added, message } = await request(
+      "POST",
+      "api/products/remove-one-from-cart",
+      body
+    );
+    if (added) console.log("implementa sti cazzo di alert", message);
+  }
   async function deleteItemFromDB() {
-    const body = {
-      product_id: item.product.id,
-    };
     const { deleted, message } = await request(
       "POST",
       "api/products/delete-from-cart",
@@ -43,7 +48,12 @@ const CartItem = ({ item }) => {
     await addItemToDB();
     setQuantity(quantity + 1);
   };
-
+  const removeOneItemFromCart = async () => {
+    if (quantity >= 2) {
+      await removeOneItemFromDB();
+      setQuantity(quantity - 1);
+    }
+  };
   const deleteItemFromCart = async () => {
     await deleteItemFromDB();
     setVisible(false);
@@ -57,7 +67,7 @@ const CartItem = ({ item }) => {
           <span className="item-description">{item.product.description}</span>
           <div className="button-container">
             <div className="add-remove-item">
-              <button className="remove-item" onClick={() => checkQuantity()}>
+              <button className="remove-item" onClick={removeOneItemFromCart}>
                 <FontAwesomeIcon icon={faMinusSquare} className="icon-size" />
               </button>
               <span className="quantity-value">{quantity}</span>
