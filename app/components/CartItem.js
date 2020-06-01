fa5a0c2b2681e05061d5e1df6bcc9ea129c2acaa
manuelpelloni <61501986+manuelpelloni@users.noreply.github.com>
2020-06-01 09:26:32 +0200

    Implemented deleting of item from cart Calling the endpoint to delete item from server Setting the visibility of item to false clicking on delete icon

--- app/components/CartItem.js
+++ app/components/CartItem.js
@@ -10,6 +10,7 @@ import request from "../utils/http";
 
 const CartItem = ({ item }) => {
   const [quantity, setQuantity] = useState(item.quantity);
+  const [visible, setVisible] = useState(true);
 
   async function addItemToDB() {
     const body = {
@@ -22,39 +23,59 @@ const CartItem = ({ item }) => {
     );
     if (added) console.log("implementa sti cazzo di alert", message);
   }
+  async function deleteItemFromDB() {
+    const body = {
+      product_id: item.product.id,
+    };
+    const { deleted, message } = await request(
+      "POST",
+      "api/products/delete-from-cart",
+      body
+    );
+    if (deleted) console.log("implementa sti cazzo di alert", message);
+  }
 
   function checkQuantity() {
     if (quantity >= 2) setQuantity(quantity - 1);
   }
 
-  const addItemToCart = () => {
-    addItemToDB();
+  const addItemToCart = async () => {
+    await addItemToDB();
     setQuantity(quantity + 1);
   };
 
+  const deleteItemFromCart = async () => {
+    await deleteItemFromDB();
+    setVisible(false);
+  };
+
   return (
-    <div className="CartItem" onClick="">
-      <span className="item-name">{item.product.name}</span>
-      <span className="item-description">{item.product.description}</span>
-      <div className="button-container">
-        <div className="add-remove-item">
-          <button className="remove-item" onClick={() => checkQuantity()}>
-            <FontAwesomeIcon icon={faMinusSquare} className="icon-size" />
-          </button>
-          <span className="quantity-value">{quantity}</span>
-          <button className="add-item" onClick={addItemToCart}>
-            <FontAwesomeIcon icon={faPlusSquare} className="icon-size" />
-          </button>
+    <>
+      {visible && (
+        <div className="CartItem" onClick="">
+          <span className="item-name">{item.product.name}</span>
+          <span className="item-description">{item.product.description}</span>
+          <div className="button-container">
+            <div className="add-remove-item">
+              <button className="remove-item" onClick={() => checkQuantity()}>
+                <FontAwesomeIcon icon={faMinusSquare} className="icon-size" />
+              </button>
+              <span className="quantity-value">{quantity}</span>
+              <button className="add-item" onClick={addItemToCart}>
+                <FontAwesomeIcon icon={faPlusSquare} className="icon-size" />
+              </button>
+            </div>
+            <div className="price-container">
+              <span>Importo</span>
+              <span className="price"> {quantity * item.product.price}€</span>
+            </div>
+            <button className="delete-item" onClick={deleteItemFromCart}>
+              <FontAwesomeIcon icon={faTrashAlt} className="icon-size" />
+            </button>
+          </div>
         </div>
-        <div className="price-container">
-          <span>Importo</span>
-          <span className="price"> {quantity * item.product.price}€</span>
-        </div>
-        <button className="delete-item">
-          <FontAwesomeIcon icon={faTrashAlt} className="icon-size" />
-        </button>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
