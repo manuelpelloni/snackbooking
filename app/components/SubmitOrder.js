f118c5636517dbbaa4199ccee9c693cc44eb9603
manuelpelloni <61501986+manuelpelloni@users.noreply.github.com>
2020-06-03 11:31:43 +0200

    Removed unused imports

--- app/components/SubmitOrder.js
+++ app/components/SubmitOrder.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import "./SubmitOrder.css";
-import request from "../utils/http";
 
 const SubmitOrder = ({ submitted }) => {
   let buttonText = submitted ? "Annulla Ordine" : "Ordina";
