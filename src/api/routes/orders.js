5b61a9e56a5ecc0215a74336dde6496fac1e4359
Manuel Pelloni <pellonimanuel@gmail.com>
2020-06-22 21:47:20 +0200

    Implemented prepared statement

--- src/api/routes/orders.js
+++ src/api/routes/orders.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../database");
+const sql = require("mssql");
 
 router.post("/submit", async (req, res) => {
   const user = await db.checkUserLogin(req, res);
   if (!user) return;
 
-  await db.createQuery().query(`UPDATE users 
+  await db.createQuery().input("user_id", sql.Int, user.user_id)
+    .query(`UPDATE users 
             SET submitted_at = GETDATE()
-            WHERE id = ${user.user_id}`);
+            WHERE id = @user_id`);
 });
 
 router.delete("/", (req, res) => {});
