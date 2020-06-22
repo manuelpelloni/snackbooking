e05c85450642ff320c1cf3dfe571bc9dd97c27a1
Manuel Pelloni <pellonimanuel@gmail.com>
2020-06-22 21:40:32 +0200

    Orders endpoint refactor Removed unused endpoints add /submit endpoint

--- src/api/routes/orders.js
+++ src/api/routes/orders.js
@@ -2,48 +2,15 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../database");
 
-router.get("/", async (req, res) => {
-  const user = await db.userFromRequest(req);
-  if (!user) return res.status(401).json({ message: "Devi prima loggarti" });
-  if (!user.admin)
-    return res.status(403).json({ message: "Non sei un amministratore" });
+router.post("/submit", async (req, res) => {
+  const user = await db.checkUserLogin(req, res);
+  if (!user) return;
 
-  const result = await db
-    .createQuery()
-    .query(
-      "SELECT CONCAT(users.class_number, users.section) AS class,\
-                    orders.id, orders.description, products.name AS product_name,\
-                    products.description AS product_description, products.price AS product_price,\
-                    orders_products.quantity, orders_products.product_id, orders.created_at\
-                FROM orders\
-                INNER JOIN orders_products ON orders_products.order_id = orders.id\
-                INNER JOIN products ON products.id = orders_products.product_id\
-                INNER JOIN users ON users.id = orders.user_id\
-                WHERE created_at >= DATEADD(DAY, DATEDIFF(DAY, 0, GETDATE()), 0)"
-    );
-  const orders = [];
-  for (const item of result.recordset) {
-    const order = orders[item.id] || {
-      id: item.id,
-      class: item.class,
-      description: item.description,
-      items: [],
-    };
-    order.items.push({
-      product: {
-        id: item.product_id,
-        name: item.product_name,
-        description: item.product_description,
-        price: item.product_price,
-      },
-      quantity: item.quantity,
-    });
-    orders[item.id] = order;
-  }
-  res.json(orders.flat(1));
+  await db.createQuery().query(`UPDATE users 
+            SET submitted_at = GETDATE()
+            WHERE id = ${user.user_id}`);
 });
 
-router.post("/", (req, res) => {});
 router.delete("/", (req, res) => {});
 router.patch("/", (req, res) => {});
 
