ac28694d639890179b07d43cb3938b605fd0eca1
Manuel Pelloni <pellonimanuel@gmail.com>
2020-06-23 03:46:36 +0200

    Add /cancel endpoint to cancel the submitted order

--- src/api/routes/orders.js
+++ src/api/routes/orders.js
@@ -7,10 +7,43 @@ router.post("/submit", async (req, res) => {
   const user = await db.checkUserLogin(req, res);
   if (!user) return;
 
-  await db.createQuery().input("user_id", sql.Int, user.user_id)
-    .query(`UPDATE users 
+  try {
+    await db.createQuery().input("user_id", sql.Int, user.user_id)
+      .query(`UPDATE users 
             SET submitted_at = GETDATE()
             WHERE id = @user_id`);
+    res.json({
+      message: "Ordine effettuato!",
+      err,
+    });
+  } catch (err) {
+    res.json({
+      message: "Non è possibile fare l'ordine",
+      err,
+    });
+  }
+});
+
+router.post("/cancel", async (req, res) => {
+  const user = await db.checkUserLogin(req, res);
+  if (!user) return;
+
+  try {
+    await db.createQuery().input("user_id", sql.Int, user.user_id)
+      .query(`UPDATE users 
+            SET submitted_at = NULL
+            WHERE id = @user_id`);
+
+    res.json({
+      message: "Ordine annullato",
+      err,
+    });
+  } catch (err) {
+    res.json({
+      message: "Non è possibile annullare l'ordine",
+      err,
+    });
+  }
 });
 
 router.delete("/", (req, res) => {});
